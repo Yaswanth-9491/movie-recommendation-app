521ef7a6943eb1cbf2d22a7a0d263bd5c1337e1c
Yaswanth-9491 <yaswanthrajmaddhi@gmail.com>
2026-06-19 06:19:49 +0530

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
@@ -7,9 +7,9 @@ import App from "./App.jsx";
 import MovieProvider from "./context/MovieContext";
 
 createRoot(document.getElementById("root")).render(
-  
+  <HashRouter>
     <MovieProvider>
       <App />
     </MovieProvider>
-  
+  </HashRouter>
 );
