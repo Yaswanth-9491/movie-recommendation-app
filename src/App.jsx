c38a4650db8cd011935864a163727976f2b5da3e
Yaswanth-9491 <yaswanthrajmaddhi@gmail.com>
2026-06-19 06:26:43 +0530

    Fix router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 
@@ -9,7 +9,7 @@ import MovieDetails from "./pages/MovieDetails";
 
 function App() {
   return (
-    <HashRouter>
+    <>
       <Navbar />
 
       <Routes>
@@ -18,7 +18,7 @@ function App() {
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
       </Routes>
-    </HashRouter>
+    </>
   );
 }
 
